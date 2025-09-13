db07f2e2ed54171ac503cae11079ba772aff2064
Iago Chiapetta <iago@DESKTOP-IFMVRHV.localdomain>
2025-09-12 22:34:00 -0300

    update 0.8

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/iago/chiapetta-cloud/cloud-storage/venv
+set -gx VIRTUAL_ENV /home/iago/cloud/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
